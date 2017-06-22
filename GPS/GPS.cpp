de6338eb2fa9d1e034e1ea4a45c549233e887a2d
20161104615 <1019172727@qq.com>
2017-06-22 21:36:35 +0800

    date

--- GPS/GPS.cpp
+++ GPS/GPS.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main(){
-    int i,n;
-    char DLOfGPRMCNumber[24];
-    char DLOfGPGGANumber[24];
+    int i,n,k;
+    char DLOfGPRMCNumber[24],DLOfGPGGANumber[24];
     FILE *f,*p;
     f = fopen ("GPS.txt","r+");
     p = fopen ("GPS.txt","r+");
-    if (f==NULL){
+    printf("请输入查询的行数：");
+    scanf("%d",&k);
+    if (f==NULL&&p==NULL){
         printf("文件打卡失败!\n");
     }
     else {
-    	for(n=0;n<9;n++){
+    	for(n=0;n<k;n++){
 	    	fseek(f, 16+n*125L, SEEK_CUR);
 	    	printf("GPRMC经纬度为：");
 	        for (i=0;i<22;i++){
@@ -19,16 +20,7 @@ int main(){
 	        }
 	        fseek(f,0L,SEEK_SET);
 	        printf("\n");
-		} 
-        fclose(f);
-    }
-    
-    if (p==NULL){
-        printf("文件打开失败!\n");
-    }
-    else {
-    	for (n=0;n<9;n++){
-	    	fseek(p, 78+n*125L, SEEK_CUR);
+	        fseek(p, 78+n*125L, SEEK_CUR);
 	    	printf("GPGGA经纬度为：");
 	        for (i=0;i<22;i++){
 	            fscanf(p,"%c",&DLOfGPGGANumber[i]);
@@ -36,7 +28,8 @@ int main(){
 	        }
 	        fseek(p,0L,SEEK_SET); 
 	        printf("\n");
-		}
+		} 
+        fclose(f);
         fclose(p);
     }
     return 0;
